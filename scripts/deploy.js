f72c41a8d21164407f7928c59f65b7c244aea71e
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2023-10-06 13:24:21 -0400

    updated deployment script for VSA contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,23 +6,41 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
+
+//   const lockedAmount = hre.ethers.parseEther("0.001");
+
+//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+//     value: lockedAmount,
+//   });
+
+//   await lock.waitForDeployment();
+
+//   console.log(
+//     `Lock with ${ethers.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+//   );
+// }
+
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Get the ContractFactory for the VSASummer23NFT contract
+  const VSASummer23NFT = await hre.ethers.getContractFactory("VSASummer23NFT");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // Address of the initial owner
+  const initialOwnerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; // Replace with the desired address
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Deploy the contract with the initial owner address
+  const nft = await VSASummer23NFT.deploy(initialOwnerAddress);
 
-  await lock.waitForDeployment();
+  // Wait for the contract to be mined
+  // await nft.deployed();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Log the address of the deployed contract
+  console.log("VSASummer23NFT deployed to:", await nft.getAddress()); 
 }
 
 // We recommend this pattern to be able to use async/await everywhere
