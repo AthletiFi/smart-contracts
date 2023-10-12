74a34b1b58e75605db8a9ed95731115bec1f9c7c
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2023-10-12 00:55:31 -0400

    updated deploy script to prompt for owner address

--- migrations/2_deploy_VSASummer23NFT.js
+++ migrations/2_deploy_VSASummer23NFT.js
@@ -2,42 +2,54 @@ const readline = require('readline');
 const VSASummer23NFT = artifacts.require("VSASummer23NFT");
 
 module.exports = function(deployer, network, accounts) {
-  return new Promise((resolve, reject) => {
-    let initialOwnerAddress;
+  const defaultAccount = "0xde3670c315cd69d81e90d3714788635aaf011860"
+
+  return new Promise(async (resolve, reject) => {
     console.log("Current network:", network);
-  
+    let initialOwnerAddress;
+    const promptOwner = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+
     // Local development configurations (both for truffle develop and external Ganache/Geth)
     if (network === "development" || network === "develop") {
       initialOwnerAddress = accounts[0];
     } 
     // Polygon testnet configuration
-    else if (network === "mumbai") {
-      initialOwnerAddress = "0xYourTestnetAddressHere";
+    else if (network === "mumbai" || network === "polygon") {
+      // initialOwnerAddress = "0xYourTestnetAddressHere";
       // You can add other configurations specific to Mumbai testnet here
-    } 
-    // Polygon mainnet configuration
-    else if (network === "polygon") {
-      initialOwnerAddress = "0xYourMainnetAddressHere";
-      // You can add other configurations specific to Polygon mainnet here
+      initialOwnerAddress = await new Promise((resolve, reject) => {
+        promptOwner.question(`Enter the inital owner address for this contract (or just press enter to use the hardcoded address): `, (ownerAddress) => {
+          promptOwner.close();
+          resolve(ownerAddress ? ownerAddress : defaultAccount);
+        });
+      });
+      // You can add other configurations specific to Polygon here
       // For example, setting a specific gas price or adding safety checks
+    } else {
+      console.error(`Unsupported network: ${network}. Please add configurations for this network in 2_deploy_VSASummer23NFT.js`);
+      reject();
     }
 
     console.log("Deploying with initial owner:", initialOwnerAddress);
   
     // Prompt for gas price if on Polygon mainnet or testnet
-    if (network === "polygon" || network === "mumbai" || true) {
-      const rl = readline.createInterface({
+    if (network === "polygon" || network === "mumbai") {
+      const gasPrompt = readline.createInterface({
         input: process.stdin,
         output: process.stdout
       });
 
-      rl.question('Enter the gas price in Gwei: ', (gasPrice) => {
+      gasPrompt.question('Enter the gas price in Gwei: ', (gasPrice) => {
         // Convert gas price from Gwei to Wei
         const gasPriceInWei = web3.utils.toWei(gasPrice, 'gwei');
-        console.log(`Using gas price of ${gasPriceInWei} Wei (${gasPrice} Gwei) for deployment on ${network} Ya FUCKIN HEARD MOFUCKA??!`);
+        console.log(`Using gas price of ${gasPriceInWei} Wei (${gasPrice} Gwei) for deployment on ${network}`);
         deployer.deploy(VSASummer23NFT, initialOwnerAddress, { gasPrice: gasPriceInWei })
           .then(() => {
-            rl.close();
+            gasPrompt.close();
             resolve();
           })
           .catch(reject);
